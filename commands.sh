eb91ec3634dfd8e4a2fa5571cf736d12780684b7
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-27 20:11:23 +0200

    bug fix aws cloudformation cli command

--- commands.sh
+++ commands.sh
@@ -2,4 +2,4 @@
 aws s3 mb s3://sam-scratch-caliandro-eu-west-1 --region eu-west-1
 
 #package cloudformation
-aws cloudformation package
+aws cloudformation package --template-file template.yaml --s3-bucket sam-scratch-caliandro-eu-west-1
