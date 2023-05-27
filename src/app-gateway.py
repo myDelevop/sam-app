b53b31bc235ffcdbfc33474307012ed59e86a571
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-27 21:03:01 +0200

    bug fixing

--- src/app-gateway.py
+++ src/app-gateway.py
@@ -5,7 +5,7 @@ print('Loading Function')
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps,
+        'body': err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json'
         },
