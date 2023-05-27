2db1c0e8719b468a7564772cce0e37909fc64bca
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-27 19:50:00 +0200

    changed py function behavior

--- src/app.py
+++ src/app.py
@@ -7,5 +7,5 @@ def lambda_handler(event, context):
     print("value 1 = " + event['key1'])
     print("value 2 = " + event['key2'])
     print("value 3 = " + event['key3'])
-    return event['key1'] #Echo back the first key value
+    return "Hello World" #Echo back the first key value
     #raise Exception("Somethin Wrong")
