a9dd46651d53ce3b1570ee54baccb0087173a25e
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-27 20:16:24 +0200

    aws cloudformation package

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #use sh in cloud cli
 aws s3 mb s3://sam-scratch-caliandro-eu-west-1 --region eu-west-1
 
-#package cloudformation
-aws cloudformation package --template-file template.yaml --s3-bucket sam-scratch-caliandro-eu-west-1
+#package cloudformation (you can also use sam cloudformation package ... )
+aws cloudformation package --template-file template.yaml --s3-bucket sam-scratch-caliandro-eu-west-1 --output-template-file gen/template-generate.yaml
